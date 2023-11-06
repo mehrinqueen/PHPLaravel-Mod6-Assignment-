aa603e6acadd55b72932af427ea5bd5ddca978d9
Fatema Mehrin Queen <45853172+mehrinqueen@users.noreply.github.com>
2023-11-06 17:38:37 +0600

    Add files via upload

---
+++ task_2.sql
@@ -0,0 +1,9 @@
+/*Retrieve the product name, quantity, and total amount for each order item. Display the result in ascending order of the order ID.*/
+SELECT
+    oi.order_id,
+    p.name AS product_name,
+    oi.quantity,
+    (oi.quantity * oi.unit_price) AS total_amount
+FROM Order_Items oi
+JOIN Products p ON oi.product_id = p.product_id
+ORDER BY oi.order_id ASC;
